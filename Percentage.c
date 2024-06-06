be2a0f6b738bbfdfa6cde16b9ec4f857a9cc6d84
ayush-989 <ayusht9189@gmail.com>
2024-06-06 05:47:11 +0530

    Add files via upload

---
+++ Percentage.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float m1 = 91; //marks of physcis
+    float m2 = 92 ;// marks of maths
+    float m3 = 78;//marks of english
+    float m4 = 84;//marks of chemistry
+    float p = (m1+m2+m3+m4)/4;//pencentage of all subject marks
+    printf("percentage:%f",p);
+    return 0 ;
+}
