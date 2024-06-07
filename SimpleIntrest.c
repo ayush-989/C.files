494bd71a0b16961e73eb84d26b1ecad5972c3137
ayush-989 <ayusht9189@gmail.com>
2024-06-07 06:18:16 +0530

    Add files via upload

---
+++ SimpleIntrest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+
+float p , r , s ,t ;
+// p = principl, r = rate , s = simple intrest , t = time
+p = 4000;
+r = 5 ;
+t = 3;  
+s = ( p*r*t)/100;
+printf("Simple intrest:%f",s);
+return 0 ;
+
+}
